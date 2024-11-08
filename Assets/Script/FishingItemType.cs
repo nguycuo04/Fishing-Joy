8394e81f31c61624289a3b6bf35c934b13f7f76f
CUONG NGUYEN <nguyenmanhcuong2303@gmail.com>
2024-11-08 10:08:43 +0700

    8th Nov

---
+++ Assets/Script/FishingItemType.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Scripts/Shop/FishingItemType.cs
+public enum FishingItemType
+{
+    Rod,
+    Bait,
+    Accessory,
+    Boat,
+    Upgrade
+}
+
